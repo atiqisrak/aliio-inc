cbb44b7d86887d3b0326ec464a76f9db0351c8b8
atiqisrak <blueniil9@gmail.com>
2022-11-20 11:54:53 +0600

    Added chat plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,36 @@ export default function Home() {
           ><Image src="/Aliio_official.png" alt="Aliio Official Logo" width={64} height={64} />
           </a>
         </span>
+        <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
 
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "105658605703131");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v15.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
       </footer>
     </div>
   )
